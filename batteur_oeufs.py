1d1b03c7a93f64d315c11c7abd1124d90e2568ee
Gilles <quodbach.gilles@gmail.com>
2025-01-23 09:50:44 +0100

    project setup

---
+++ batteur_oeufs.py
@@ -0,0 +1,14 @@
+import threading
+import time
+
+
+class BatteurOeufs(threading.Thread):
+
+    def __init__(self, nb_oeufs):
+        threading.Thread.__init__(self)
+        self.nb_oeufs = nb_oeufs
+    def run(self):
+        nb_tours = self.nb_oeufs * 8
+        for no_tour in range(1, nb_tours + 1):
+            print(f"\tJe bats les {self.nb_oeufs} oeufs, tour n°{no_tour}")
+            time.sleep(0.5) # temps supposé d'un tour de batteur
